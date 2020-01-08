f0a8e944e2630338944caa92620c830c35d35d31
Ilari Tommiska <ilari97@hotmail.com>
2020-01-08 14:20:10 +0200

    Removed the deprecated DamageInstance constructor

--- game/src/main/java/fi/jakojaannos/roguelite/game/data/DamageInstance.java
+++ game/src/main/java/fi/jakojaannos/roguelite/game/data/DamageInstance.java
@@ -6,9 +6,4 @@ import lombok.RequiredArgsConstructor;
 public class DamageInstance {
     public final double damage;
     public final DamageSource<?> source;
-
-    @Deprecated
-    public DamageInstance(double damage) {
-        this(damage, DamageSource.Generic.UNDEFINED);
-    }
 }
