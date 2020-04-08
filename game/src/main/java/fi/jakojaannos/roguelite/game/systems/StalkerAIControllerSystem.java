26e4beb70b61080c0d1e7e62d7e588eb2529c140
toilari <ilari97@hotmail.com>
2020-04-08 16:28:51 +0300

    Refactored StalkerAI controller to new ECS

--- game/src/main/java/fi/jakojaannos/roguelite/game/systems/StalkerAIControllerSystem.java
+++ game/src/main/java/fi/jakojaannos/roguelite/game/systems/StalkerAIControllerSystem.java
@@ -6,11 +6,9 @@ import java.util.Random;
 import java.util.stream.Stream;
 
 import fi.jakojaannos.roguelite.engine.data.components.Transform;
-import fi.jakojaannos.roguelite.engine.ecs.World;
-import fi.jakojaannos.roguelite.engine.ecs.legacy.ECSSystem;
-import fi.jakojaannos.roguelite.engine.ecs.legacy.Entity;
-import fi.jakojaannos.roguelite.engine.ecs.legacy.EntityManager;
-import fi.jakojaannos.roguelite.engine.ecs.legacy.RequirementsBuilder;
+import fi.jakojaannos.roguelite.engine.ecs.EcsSystem;
+import fi.jakojaannos.roguelite.engine.ecs.Requirements;
+import fi.jakojaannos.roguelite.engine.ecs.Without;
 import fi.jakojaannos.roguelite.engine.utilities.TimeManager;
 import fi.jakojaannos.roguelite.game.data.components.InAir;
 import fi.jakojaannos.roguelite.game.data.components.Physics;
@@ -19,117 +17,98 @@ import fi.jakojaannos.roguelite.game.data.components.character.WalkingMovementAb
 import fi.jakojaannos.roguelite.game.data.components.character.enemy.StalkerAI;
 import fi.jakojaannos.roguelite.game.data.resources.Players;
 
-public class StalkerAIControllerSystem implements ECSSystem {
+public class StalkerAIControllerSystem implements EcsSystem<StalkerAIControllerSystem.Resources, StalkerAIControllerSystem.EntityData, EcsSystem.NoEvents> {
     private final Random random = new Random(8055); // 8055 = 666 + 420 + 6969
     private final Vector2d tempForce = new Vector2d();
     private final Vector2d emptyPos = new Vector2d();
 
     @Override
-    public void declareRequirements(final RequirementsBuilder requirements) {
-        requirements.addToGroup(SystemGroups.INPUT)
-                    .requireResource(Players.class)
-                    .withComponent(StalkerAI.class)
-                    .withComponent(MovementInput.class)
-                    .withComponent(Transform.class)
-                    .withComponent(WalkingMovementAbility.class)
-                    .withComponent(Physics.class)
-                    .withoutComponent(InAir.class);
+    public Requirements<Resources, EntityData, NoEvents> declareRequirements(
+            final Requirements<Resources, EntityData, NoEvents> require
+    ) {
+        return require.entityData(EntityData.class)
+                      .resources(Resources.class);
     }
 
     @Override
     public void tick(
-            final Stream<Entity> entities,
-            final World world
+            final Resources resources,
+            final Stream<EntityDataHandle<EntityData>> entities,
+            final NoEvents noEvents
     ) {
-        final var entityManager = world.getEntityManager();
-        final var timeManager = world.fetchResource(TimeManager.class);
-        final var optPlayer = world.fetchResource(Players.class).getLocalPlayer();
-        final var playerPos = optPlayer.flatMap(player -> player.getComponent(Transform.class))
-                                       .map(transform -> transform.position)
-                                       .orElse(this.emptyPos);
+        final var maybePlayer = resources.players.getLocalPlayer();
+        if (maybePlayer.isEmpty()) {
+            entities.forEach(this::doWanderMovement);
+            return;
+        }
+
+        final var playerPos = maybePlayer.flatMap(player -> player.getComponent(Transform.class))
+                                         .map(transform -> transform.position)
+                                         .orElse(this.emptyPos);
 
         entities.forEach(entity -> {
-            final var stalkerAI = entityManager.getComponentOf(entity, StalkerAI.class).orElseThrow();
-            final var characterInput = entityManager.getComponentOf(entity, MovementInput.class).orElseThrow();
-            final var characterStats = entityManager.getComponentOf(entity, WalkingMovementAbility.class).orElseThrow();
-            final var physics = entityManager.getComponentOf(entity, Physics.class).orElseThrow();
-            final var myPos = entityManager.getComponentOf(entity, Transform.class).orElseThrow();
+            final var ai = entity.getData().stalkerAi;
+            final var input = entity.getData().input;
+            final var ability = entity.getData().ability;
+            final var transform = entity.getData().transform;
 
-            if (optPlayer.isEmpty()) {
-                doWanderMovement(characterInput, characterStats, stalkerAI);
-                return;
-            }
+            final var distToPlayerSquared = transform.position.distanceSquared(playerPos);
 
-            final var distToPlayerSquared = myPos.position.distanceSquared(playerPos);
-
-            if (distToPlayerSquared <= stalkerAI.leapRadiusSquared) {
-                doCloseRangeMovement(characterInput,
-                                     entityManager,
-                                     entity,
-                                     characterStats,
-                                     physics,
-                                     stalkerAI,
-                                     timeManager,
-                                     myPos.position,
+            if (distToPlayerSquared <= ai.leapRadiusSquared) {
+                doCloseRangeMovement(entity,
+                                     ai,
+                                     resources.timeManager,
+                                     transform.position,
                                      playerPos);
-            } else if (distToPlayerSquared <= stalkerAI.aggroRadiusSquared) {
-                sneakTowardsPlayer(characterInput, characterStats, stalkerAI, myPos.position, playerPos);
+            } else if (distToPlayerSquared <= ai.aggroRadiusSquared) {
+                sneakTowardsPlayer(input, ability, ai, transform.position, playerPos);
             } else {
-                doWanderMovement(characterInput, characterStats, stalkerAI);
+                doWanderMovement(entity);
             }
         });
     }
 
-    public void doWanderMovement(
-            final MovementInput movementInput,
-            final WalkingMovementAbility stats,
-            final StalkerAI ai
-    ) {
-        stats.maxSpeed = ai.moveSpeedWalk;
+    public void doWanderMovement(final EntityDataHandle<EntityData> entity) {
+        final var ability = entity.getData().ability;
+        final var ai = entity.getData().stalkerAi;
+        final var input = entity.getData().input;
+
+        ability.maxSpeed = ai.moveSpeedWalk;
         if (this.random.nextInt(40) == 0) {
-            movementInput.move.set(
-                    this.random.nextDouble() * 2 - 1.0,
-                    this.random.nextDouble() * 2 - 1.0
-            );
+            input.move.set(this.random.nextDouble() * 2 - 1.0,
+                           this.random.nextDouble() * 2 - 1.0);
         }
     }
 
     public void doCloseRangeMovement(
-            final MovementInput movementInput,
-            final EntityManager entityManager,
-            final Entity entity,
-            final WalkingMovementAbility stats,
-            final Physics physics,
+            final EntityDataHandle<EntityData> entity,
             final StalkerAI ai,
             final TimeManager time,
             final Vector2d myPos,
             final Vector2d playerPos
     ) {
         if (time.getCurrentGameTime() > ai.lastJumpTimeStamp + ai.jumpCoolDownInTicks) {
-            leapTowardsPlayer(movementInput, entityManager, entity, physics, ai, time, myPos, playerPos);
+            leapTowardsPlayer(entity, ai, time, myPos, playerPos);
         } else {
-            stats.maxSpeed = ai.moveSpeedWalk;
-            movementInput.move.set(playerPos).sub(myPos);
+            entity.getData().ability.maxSpeed = ai.moveSpeedWalk;
+            entity.getData().input.move.set(playerPos).sub(myPos);
         }
     }
 
     public void leapTowardsPlayer(
-            final MovementInput movementInput,
-            final EntityManager entityManager,
-            final Entity entity,
-            final Physics physics,
+            final EntityDataHandle<EntityData> entity,
             final StalkerAI ai,
             final TimeManager time,
             final Vector2d myPos,
             final Vector2d playerPos
     ) {
-        movementInput.move.set(0.0);
+        entity.getData().input.move.set(0.0);
         ai.lastJumpTimeStamp = time.getCurrentGameTime();
-        entityManager.addComponentTo(entity, new InAir(time.getCurrentGameTime(), ai.jumpDurationInTicks));
+        entity.addComponent(new InAir(time.getCurrentGameTime(), ai.jumpDurationInTicks));
         this.tempForce.set(playerPos)
                       .sub(myPos)
                       .normalize(ai.moveSpeedJump);
-        physics.applyForce(this.tempForce);
+        entity.getData().physics.applyForce(this.tempForce);
     }
 
     public void sneakTowardsPlayer(
@@ -143,4 +122,17 @@ public class StalkerAIControllerSystem implements ECSSystem {
         movementInput.move.set(playerPos).sub(myPos);
     }
 
+    public static record Resources(
+            Players players,
+            TimeManager timeManager
+    ) {}
+
+    public static record EntityData(
+            StalkerAI stalkerAi,
+            MovementInput input,
+            WalkingMovementAbility ability,
+            Transform transform,
+            Physics physics,
+            @Without InAir notInAir
+    ) {}
 }
