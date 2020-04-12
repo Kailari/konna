06fd67109e94f00b52433ce62a499aa1fe0ae978
toilari <ilari97@hotmail.com>
2020-04-12 14:43:31 +0300

    Preliminary documentation for test utilities

--- engine.assert/src/main/java/fi/jakojaannos/roguelite/engine/utilities/assertions/world/EntityExpect.java
+++ engine.assert/src/main/java/fi/jakojaannos/roguelite/engine/utilities/assertions/world/EntityExpect.java
@@ -1,7 +1,30 @@
 package fi.jakojaannos.roguelite.engine.utilities.assertions.world;
 
+/**
+ * Perform expectation assertions over state of an entity and its components.
+ */
 public interface EntityExpect {
+    /**
+     * Asserts that an entity has a component of given class. Additionally, if the assertion passes, creates a component
+     * matcher for performing further assertions over that component, if desired.
+     * <p>
+     * The assertion fails if the component does not exist.
+     *
+     * @param componentClass class of the component to expect to exist
+     * @param <TComponent>   type of the component
+     *
+     * @return Component matcher for asserting over the state of the component
+     *
+     * @see ComponentMatcher
+     */
     <TComponent> ComponentMatcher<TComponent> toHaveComponent(Class<TComponent> componentClass);
 
+    /**
+     * Asserts that the entity does not have a component of the given class.
+     * <p>
+     * The assertion fails if the component exists.
+     *
+     * @param componentClass class of the unexpected component
+     */
     void toNotHaveComponent(Class<?> componentClass);
 }
--- engine.assert/src/main/java/fi/jakojaannos/roguelite/engine/utilities/assertions/world/GameExpect.java
+++ engine.assert/src/main/java/fi/jakojaannos/roguelite/engine/utilities/assertions/world/GameExpect.java
@@ -4,11 +4,33 @@ import fi.jakojaannos.roguelite.engine.ecs.EntityHandle;
 import fi.jakojaannos.roguelite.engine.utilities.assertions.world.builder.SimulationBuilderImpl;
 import fi.jakojaannos.roguelite.engine.utilities.assertions.world.entity.EntityExpectImpl;
 
+/**
+ * Utilities for creating test game simulators and performing assertions on Game/ECS worlds.
+ */
 public final class GameExpect {
+    private GameExpect() {
+    }
+
+    /**
+     * Start a new test simulation builder. Name comes from the builder format which follows fluent syntax for <i>"When
+     * game with X and Y runs for Z, expect W"</i>
+     *
+     * @return the builder
+     */
     public static SimulationBuilder whenGame() {
         return new SimulationBuilderImpl();
     }
 
+    /**
+     * Start a new assertion builder for the given entity. Allows performing assertions on the entity and the state of
+     * its components.
+     *
+     * @param entity the entity to perform assertions on
+     *
+     * @return the assertion builder
+     *
+     * @see EntityExpect
+     */
     public static EntityExpect expectEntity(final EntityHandle entity) {
         return new EntityExpectImpl(entity);
     }
