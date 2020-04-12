42445f24203c339638cd48526839a38766077450
toilari <ilari97@hotmail.com>
2020-04-12 14:43:31 +0300

    Preliminary documentation for test utilities

--- engine.assert/src/main/java/fi/jakojaannos/roguelite/engine/utilities/assertions/world/ComponentMatcher.java
+++ engine.assert/src/main/java/fi/jakojaannos/roguelite/engine/utilities/assertions/world/ComponentMatcher.java
@@ -2,9 +2,24 @@ package fi.jakojaannos.roguelite.engine.utilities.assertions.world;
 
 import java.util.function.Consumer;
 
+/**
+ * Perform expectation assertions on state of a component.
+ *
+ * @param <TComponent> type of the component being matched
+ */
 public interface ComponentMatcher<TComponent> {
+    /**
+     * Expects the given expectation to not throw an assertion error upon execution.
+     *
+     * @param expectation expectation to perform
+     */
     void which(Consumer<TComponent> expectation);
 
+    /**
+     * Alias for {@link #which(Consumer)}
+     *
+     * @param expectation expectation to perform
+     */
     default void that(final Consumer<TComponent> expectation) {
         which(expectation);
     }
