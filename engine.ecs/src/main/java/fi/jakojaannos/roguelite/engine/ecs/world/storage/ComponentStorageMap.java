afec04b533afabcc8f9594d853431aa0577e15cd
toilari <ilari97@hotmail.com>
2020-04-12 10:04:47 +0300

    Removed some unused leftover API

--- engine.ecs/src/main/java/fi/jakojaannos/roguelite/engine/ecs/world/storage/ComponentStorageMap.java
+++
@@ -1,9 +0,0 @@
-package fi.jakojaannos.roguelite.engine.ecs.world.storage;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class ComponentStorageMap {
-    private final Map<Class<?>, List<?>> storages = new HashMap<>();
-}
