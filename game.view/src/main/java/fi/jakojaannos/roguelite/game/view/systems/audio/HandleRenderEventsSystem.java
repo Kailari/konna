c5d98e56a822f7dfb212ad8d205f553fa7b2353a
toilari <ilari97@hotmail.com>
2020-04-17 16:14:50 +0300

    Added shotgun variant

--- game.view/src/main/java/fi/jakojaannos/roguelite/game/view/systems/audio/HandleRenderEventsSystem.java
+++ game.view/src/main/java/fi/jakojaannos/roguelite/game/view/systems/audio/HandleRenderEventsSystem.java
@@ -1,7 +1,6 @@
 package fi.jakojaannos.roguelite.game.view.systems.audio;
 
 import java.nio.file.Path;
-import java.util.Random;
 import java.util.stream.Stream;
 
 import fi.jakojaannos.roguelite.engine.ecs.EcsSystem;
@@ -12,25 +11,17 @@ import fi.jakojaannos.roguelite.game.view.data.AudioContext;
 import fi.jakojaannos.roguelite.game.view.data.SoundEffect;
 
 public class HandleRenderEventsSystem implements EcsSystem<HandleRenderEventsSystem.Resources, EcsSystem.NoEntities, EcsSystem.NoEvents>, AutoCloseable {
-    private final SoundEffect[] shotgun;
+    private final SoundEffect shotgun;
     private final SoundEffect melee;
     private final SoundEffect gatling;
-
-    private final Random random = new Random();
-
-    public SoundEffect getRandomShotgunBlast() {
-        return this.shotgun[this.random.nextInt(this.shotgun.length)];
-    }
+    private final SoundEffect rifle;
 
     public HandleRenderEventsSystem(
             final Path assetRoot,
             final AudioContext context
     ) {
-        this.shotgun = new SoundEffect[]{
-                new SoundEffect(assetRoot, "shotgun/Blast1.ogg", context),
-                new SoundEffect(assetRoot, "shotgun/Blast2.ogg", context),
-                new SoundEffect(assetRoot, "shotgun/Blast3.ogg", context),
-        };
+        this.shotgun = new SoundEffect(assetRoot, "shotgun/Blast1.ogg", context);
+        this.rifle = new SoundEffect(assetRoot, "shotgun/Blast3.ogg", context);
         this.melee = new SoundEffect(assetRoot, "shotgun/Pump3.ogg", context);
         this.gatling = new SoundEffect(assetRoot, "shotgun/Blast2.ogg", context);
     }
@@ -44,7 +35,8 @@ public class HandleRenderEventsSystem implements EcsSystem<HandleRenderEventsSys
         resources.events.events().forEach(event -> {
             if (event instanceof GunshotEvent gunshot) {
                 switch (gunshot.variant()) {
-                    case SHOTGUN -> getRandomShotgunBlast().play(2, 0.75f, 1.5f);
+                    case SHOTGUN -> this.rifle.play(2, 0.75f, 1.0f);
+                    case RIFLE -> this.rifle.play(2, 0.75f, 1.5f);
                     case MELEE -> this.melee.play(1, 1.0f, 0.5f);
                     case GATLING -> this.gatling.play(0, 0.3f, 2.0f);
                 }
@@ -54,9 +46,8 @@ public class HandleRenderEventsSystem implements EcsSystem<HandleRenderEventsSys
 
     @Override
     public void close() {
-        for (final var soundEffect : this.shotgun) {
-            soundEffect.close();
-        }
+        this.rifle.close();
+        this.shotgun.close();
         this.melee.close();
         this.gatling.close();
     }
--- game/src/main/java/fi/jakojaannos/roguelite/game/data/events/render/GunshotEvent.java
+++ game/src/main/java/fi/jakojaannos/roguelite/game/data/events/render/GunshotEvent.java
@@ -2,8 +2,9 @@ package fi.jakojaannos.roguelite.game.data.events.render;
 
 public record GunshotEvent(Variant variant) {
     public enum Variant {
-        SHOTGUN,
+        RIFLE,
         MELEE,
         GATLING,
+        SHOTGUN,
     }
 }
